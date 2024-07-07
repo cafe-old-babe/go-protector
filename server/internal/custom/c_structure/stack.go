7cf3c0a0e9b56420b4852c1f36b4bc8dc555f0bf
cafe-old-babe <cafe-old-babe@qq.com>
2024-07-07 23:09:57 +0800

    mutex

--- server/internal/custom/c_structure/stack.go
+++ server/internal/custom/c_structure/stack.go
@@ -4,12 +4,12 @@ import "sync"
 
 type SafeStack[T any] struct {
 	stack []T
-	mutex sync.Mutex
+	sync.Mutex
 }
 
 func (_self *SafeStack[T]) Push(data T) {
-	_self.mutex.Lock()
-	defer _self.mutex.Unlock()
+	_self.Lock()
+	defer _self.Unlock()
 	_self.stack = append(_self.stack, data)
 }
 
@@ -17,8 +17,8 @@ func (_self *SafeStack[T]) Pop() (data T, exists bool) {
 	if exists = !_self.IsEmpty(); !exists {
 		return
 	}
-	_self.mutex.Lock()
-	defer _self.mutex.Unlock()
+	_self.Lock()
+	defer _self.Unlock()
 
 	i := len(_self.stack) - 1
 	data = _self.stack[i]
@@ -32,21 +32,21 @@ func (_self *SafeStack[T]) Top() (data T, exists bool) {
 	if exists = !_self.IsEmpty(); !exists {
 		return
 	}
-	_self.mutex.Lock()
-	defer _self.mutex.Unlock()
+	_self.Lock()
+	defer _self.Unlock()
 	data = _self.stack[len(_self.stack)-1]
 	return
 }
 
 func (_self *SafeStack[T]) IsEmpty() bool {
-	_self.mutex.Lock()
-	defer _self.mutex.Unlock()
+	_self.Lock()
+	defer _self.Unlock()
 	return len(_self.stack) <= 0
 }
 
 func (_self *SafeStack[T]) Clear() {
-	_self.mutex.Lock()
-	defer _self.mutex.Unlock()
+	_self.Lock()
+	defer _self.Unlock()
 	_self.stack = _self.stack[:0]
 	return
 }
--- server/internal/ssh/cmd/cursor.go
+++ server/internal/ssh/cmd/cursor.go
@@ -3,13 +3,13 @@ package cmd
 import "sync"
 
 type cursor struct {
-	x, y  int
-	mutex sync.Mutex
+	x, y int
+	sync.Mutex
 }
 
 func (_self *cursor) MoveX(i int) int {
-	_self.mutex.Lock()
-	defer _self.mutex.Unlock()
+	_self.Lock()
+	defer _self.Unlock()
 
 	_self.x += i
 	if _self.x < 0 {
@@ -19,8 +19,8 @@ func (_self *cursor) MoveX(i int) int {
 }
 
 func (_self *cursor) MoveY(i int) int {
-	_self.mutex.Lock()
-	defer _self.mutex.Unlock()
+	_self.Lock()
+	defer _self.Unlock()
 	_self.y += i
 	if _self.y < 0 {
 		_self.y = 0
@@ -45,20 +45,20 @@ func (_self *cursor) SetY(i int) {
 }
 
 func (_self *cursor) ResetX() {
-	_self.mutex.Lock()
-	defer _self.mutex.Unlock()
+	_self.Lock()
+	defer _self.Unlock()
 	_self.x = 0
 }
 
 func (_self *cursor) ResetY() {
-	_self.mutex.Lock()
-	defer _self.mutex.Unlock()
+	_self.Lock()
+	defer _self.Unlock()
 	_self.y = 0
 }
 
 func (_self *cursor) ResetCursor() {
-	_self.mutex.Lock()
-	defer _self.mutex.Unlock()
+	_self.Lock()
+	defer _self.Unlock()
 	_self.y = 0
 	_self.x = 0
 
